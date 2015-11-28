701e37a8b8eeee29551a8461fc5b9c78d1074c6c
Aaron Hunter <aaron@sharknet.us>
2015-11-28 09:37:40 -0500

    Added documentation to the Complex example.

--- src/main/java/us/sharknet/dota/model/example/Complex.java
+++ src/main/java/us/sharknet/dota/model/example/Complex.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 
 /**
  * Complex examples that show how to handle the problem of objects having different properties.
+ * I use the Apache BeanUtils library to filter classes by their properties.
  */
 public class Complex {
 
@@ -23,7 +24,9 @@ public class Complex {
     }
 
     public void printAgilityItems() {
+        //Create a predicate that returns true if the item has a property that affects a hero's agility
         AgilityItemPredicate agilityItemPredicate = new AgilityItemPredicate();
+        //Create a filtered collection of only agility related items
         Collection<Item> agilityItems = CollectionUtils.select( library.getItems(), agilityItemPredicate );
         System.out.println("Items that provide agility");
         for( Item item : agilityItems) {
